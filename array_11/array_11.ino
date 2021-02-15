a327d0e30119c418b16b70d3f6e6d5c222b7cfa6
Jeff <aropeak@users.noreply.github.com>
2021-02-15 13:20:19 -0500

    array 11

---
+++ array_11/array_11.ino
@@ -0,0 +1,37 @@
+#include <Adafruit_NeoPixel.h>
+#include "frames.h"
+Adafruit_NeoPixel strip(64, 6);
+
+int colors[5][3] {
+  {0, 0, 0},
+  {0, 255, 0},
+  {255, 0, 0},
+  {0, 0, 255},
+  {255, 255, 0},
+};
+
+int whichFrame = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  strip.begin();
+  strip.clear();
+  strip.setBrightness(50);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < strip.numPixels(); i++) {
+    int whichColor = animation[whichFrame][i];
+
+    int red = colors[whichColor][0];
+    int green = colors[whichColor][1];
+    int blue = colors[whichColor][2];
+
+    strip.setPixelColor(i, red, green, blue);
+  }
+  strip.show();
+  whichFrame = (whichFrame + 1) % 4;
+  delay(200);
+}
