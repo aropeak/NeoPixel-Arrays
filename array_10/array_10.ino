be85a42dd4a22dfaa994dd21753add30a3d2a1b2
Jeff <aropeak@users.noreply.github.com>
2021-02-15 09:42:17 -0500

    array 10 and array 10 piskel gif

--- array_10/array_10.ino
+++ array_10/array_10.ino
@@ -2,11 +2,12 @@
 #include "frames.h"
 Adafruit_NeoPixel strip(64, 6);
 
-int colors[4][3] {
+int colors[5][3] {
   {0, 0, 0},
-  {0, 0, 255},
+  {0, 255, 0},
   {255, 0, 0},
-  {0, 255, 0}
+  {255, 255, 255},
+  {0, 0, 255},
 };
 
 int whichFrame = 0;
@@ -23,14 +24,14 @@ void loop() {
   // put your main code here, to run repeatedly:
   for (int i = 0; i < strip.numPixels(); i++) {
     int whichColor = animation[whichFrame][i];
-    
+
     int red = colors[whichColor][0];
     int green = colors[whichColor][1];
     int blue = colors[whichColor][2];
-    
+
     strip.setPixelColor(i, red, green, blue);
   }
   strip.show();
-  whichFrame = (whichFrame +1) % 3;
-  delay(500);
+  whichFrame = (whichFrame + 1) % 5;
+  delay(200);
 }
